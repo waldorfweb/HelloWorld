2c1ecfd64d88323a4458328109734db14a3aceb3
waldorfweb <alex@waldorfshop.eu>
2020-04-28 14:24:04 +0200

    Update HelloWorldRouteServiceProvider.php

--- src/Providers/HelloWorldRouteServiceProvider.php
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -15,7 +15,7 @@ class HelloWorldRouteServiceProvider extends RouteServiceProvider
 	 */
 	public function map(Router $router)
 	{
-		$router->get('hello', 'HelloWorld\Controllers\ContentController@sayHello');
+		//$router->get('hello', 'HelloWorld\Controllers\ContentController@sayHello');
 	}
 
 }
